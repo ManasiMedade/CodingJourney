cc304f8da551004e22f6f2d35572d7eccdb28b47
ManasiM02 <155082100+ManasiMedade@users.noreply.github.com>
2026-02-26 22:48:15 +0530

    Chapter 2 complete - strings and variables

---
+++ CodingJourney/week1.py/chapter2_practice.py
@@ -0,0 +1,14 @@
+# Variables
+spam = 'Hello'
+print(spam)
+
+# String concatenation
+name = 'Manasi'
+age = 25
+print('My name is ' + name + '. I am ' + str(age) + ' years old.')
+
+# String methods
+print('Hello'.upper())
+print('Hello'.lower())
+print('Hello'.islower())
+print('HELLO'.islower())
